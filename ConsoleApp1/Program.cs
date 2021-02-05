8fa34ed27090e83eeea8c884d8002ea36d0f149f
gr691_aas <gr691_aas@L308-6>
2021-02-05 11:37:05 +0700

    всем привет

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DADA
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine("Число является четным и кратно 10.");
+            else if (number % 2 == 0)
+                Console.WriteLine("Число является чётным.");
+            else if (number % 2 == 0)
+                Console.WriteLine("Число кратно 10.");
+            else
+                Console.WriteLine("Число является нечетным и не кратно 10.");
+
+            Console.ReadKey();
         }
     }
 }
